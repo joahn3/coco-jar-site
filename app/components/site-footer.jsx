f9b4080a258ade8d82e59d197fdfd4033facfe6f
Ionut Francisc <ionut.francisc@icloud.com>
2026-08-18 12:48:15 +0300

    feat: add analytics attrs to footer links

--- app/components/site-footer.jsx
+++ app/components/site-footer.jsx
@@ -35,17 +35,29 @@ export default function SiteFooter({ config }) {
           <p className="font-semibold text-ink-title">Meniu și rezervări</p>
           <div className="jar-link-list">
             <p>
-              <Link href="/termeni-si-conditii" className="jar-link touch-target">
+              <Link
+                href="/termeni-si-conditii"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_terms|source_page=global|journey_stage=information|lead_type=site"
+              >
                 Termeni și condiții
               </Link>
             </p>
             <p>
-              <Link href="/politica-confidentialitate" className="jar-link touch-target">
+              <Link
+                href="/politica-confidentialitate"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_privacy|source_page=global|journey_stage=information|lead_type=site"
+              >
                 Politica de confidențialitate
               </Link>
             </p>
             <p>
-              <Link href="/cookies" className="jar-link touch-target">
+              <Link
+                href="/cookies"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_cookies|source_page=global|journey_stage=information|lead_type=site"
+              >
                 Politica cookie-urilor
               </Link>
             </p>
@@ -55,17 +67,26 @@ export default function SiteFooter({ config }) {
                 href={config.social?.googleBusiness || "#"}
                 target="_blank"
                 rel="noreferrer"
+                data-analytics="click|conversion|footer_google_maps|source_page=global|journey_stage=information|lead_type=site"
               >
                 Google Maps
               </a>
             </p>
             <p>
-              <Link href="/contact" className="jar-link touch-target">
+              <Link
+                href="/contact"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_contact|source_page=global|journey_stage=lead_capture|lead_type=reservation"
+              >
                 Contact
               </Link>
             </p>
             <p>
-              <Link href="/meniu-zilei" className="jar-link touch-target">
+              <Link
+                href="/meniu-zilei"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_daily_menu|source_page=global|journey_stage=menu_cta|lead_type=menu"
+              >
                 Meniul zilei
               </Link>
             </p>
@@ -80,6 +101,7 @@ export default function SiteFooter({ config }) {
                 className="jar-link touch-target"
                 target="_blank"
                 rel="noreferrer"
+                data-analytics="click|navigation|footer_facebook|source_page=global|journey_stage=social|lead_type=site"
               >
                 Facebook
               </a>
@@ -90,6 +112,7 @@ export default function SiteFooter({ config }) {
                 className="jar-link touch-target"
                 target="_blank"
                 rel="noreferrer"
+                data-analytics="click|navigation|footer_instagram|source_page=global|journey_stage=social|lead_type=site"
               >
                 Instagram
               </a>
@@ -98,17 +121,26 @@ export default function SiteFooter({ config }) {
               <a
                 href="/meniu/coco-jar-meniu-detaliat-2026-08-15.pdf"
                 className="jar-link touch-target"
+                data-analytics="click|navigation|footer_menu_pdf|source_page=global|journey_stage=menu_cta|lead_type=menu"
               >
                 Meniu PDF
               </a>
             </p>
             <p>
-              <Link href="/meniu" className="jar-link touch-target">
+              <Link
+                href="/meniu"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_full_menu|source_page=global|journey_stage=menu_cta|lead_type=menu"
+              >
                 Meniu a la carte
               </Link>
             </p>
             <p>
-              <Link href="/evenimente-catering" className="jar-link touch-target">
+              <Link
+                href="/evenimente-catering"
+                className="jar-link touch-target"
+                data-analytics="click|navigation|footer_events|source_page=global|journey_stage=lead_capture|lead_type=event"
+              >
                 Evenimente și catering
               </Link>
             </p>
